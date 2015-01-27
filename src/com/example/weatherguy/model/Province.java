5a1b68b48dc718538a34aadca3fc6d4103d5ac25
NBchitu <bjtu2010@hotmail.com>
2015-01-27 16:44:45 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ src/com/example/weatherguy/model/Province.java
@@ -0,0 +1,29 @@
+package com.example.weatherguy.model;
+
+public class Province {
+	private int id;
+	private String provinceName;
+	private String provinceCode;
+	
+	public int getId() {
+		return id;
+		
+	}
+	public void setId(int id){
+		this.id = id; 
+	} 
+	public String getProvinceName(){
+		return provinceName;
+	}
+	
+	public void setProvinceName(String name) {
+		this.provinceName = name;
+		
+	}
+	public String getProvinceCode(){
+		return provinceCode;
+	}
+	public void setProvinceCode(String code) {
+		this.provinceCode = code;
+	}
+}
